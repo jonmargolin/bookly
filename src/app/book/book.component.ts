a7db41dfb5c65623c2eb7caae2b8c6d1569b4c21
jon margolin <jonmargolin@gmail.com>
2018-03-20 23:29:56 +0200

    set  book http

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-book',
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class BookComponent implements OnInit {
 
   constructor() { }
-
+  @Input() book:any;
+  
   ngOnInit() {
+  
   }
 
 }
